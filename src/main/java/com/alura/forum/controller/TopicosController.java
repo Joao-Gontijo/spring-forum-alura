e5549fdae5b956f940b85620a52d8a85f00687b2
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-01 23:38:40 -0300

    topicos dto

--- src/main/java/com/alura/forum/controller/TopicosController.java
+++ src/main/java/com/alura/forum/controller/TopicosController.java
@@ -6,19 +6,21 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.alura.forum.controller.dto.TopicoDto;
 import com.alura.forum.modelo.Curso;
 import com.alura.forum.modelo.Topico;
 
-@Controller
+
+@RestController //inibe a necessidade do ResponseBody
 public class TopicosController {
 	
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista(){
+	public List<TopicoDto> lista(){
 		Topico topico = new Topico("Duvida", "Id nulo", 
 				new Curso("Spring", "Programação"));
 		
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 	}
 }
