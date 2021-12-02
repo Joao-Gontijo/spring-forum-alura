ce5e0dbde8c228c791a89110934dab0ab22704c4
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-02 16:38:43 -0300

    busca filtrando por nome

--- src/main/java/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,14 @@
 package com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+		
+		//Curso é a entidade e Nome é o atributo dentro dela
+	List<Topico> findByCurso_Nome(String nomeCurso);
 
 }
