eeb2640a8f3dad44208572c00d5bf15c7a86deda
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-02 16:17:36 -0300

    criando topico repository

--- src/main/java/com/alura/forum/controller/TopicosController.java
+++ src/main/java/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,7 @@ package com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alura.forum.controller.dto.TopicoDto;
 import com.alura.forum.modelo.Curso;
 import com.alura.forum.modelo.Topico;
+import com.alura.forum.repository.TopicoRepository;
 
 
 @RestController //inibe a necessidade do ResponseBody
 public class TopicosController {
 	
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Duvida", "Id nulo", 
-				new Curso("Spring", "Programação"));
+		List<Topico> topicos = topicoRepository.findAll();
 		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+		return TopicoDto.converter(topicos);
 	}
 }
