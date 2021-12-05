058e7977aa4e60a7ccf13fe62ab84d664b5e9702
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-04 21:31:21 -0300

    atualizando tópicos

--- src/main/java/com/alura/forum/controller/TopicosController.java
+++ src/main/java/com/alura/forum/controller/TopicosController.java
@@ -7,9 +7,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import com.alura.forum.controller.dto.DetalhesTopicoDto;
 import com.alura.forum.controller.dto.TopicoDto;
+import com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import com.alura.forum.controller.form.TopicoForm;
 import com.alura.forum.modelo.Topico;
 import com.alura.forum.repository.CursoRepository;
@@ -60,5 +63,14 @@ public class TopicosController {
 		Topico topico = topicoRepository.getById(id);
 		DetalhesTopicoDto dto = new DetalhesTopicoDto(topico);
 		return dto;
+	}
+	
+	@PutMapping("/{id}")
+	//dispara o update no banco de dados
+	@Transactional 
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, 
+			@RequestBody @Valid AtualizacaoTopicoForm form){
+		Topico topico = form.atualizar(id, topicoRepository);
+		return ResponseEntity.ok(new TopicoDto(topico));
 	}
 }
