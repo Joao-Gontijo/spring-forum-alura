512b18465dd356001cfa2765f953e933628f1901
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-07 14:14:11 -0300

    invalidando cache desatualizado

--- src/main/java/com/alura/forum/controller/TopicosController.java
+++ src/main/java/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -66,6 +67,8 @@ public class TopicosController {
 	}
 
 	@PostMapping //RequestBody é pra pegar do corpo da requisição
+	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true) //limpa o cache
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -88,6 +91,7 @@ public class TopicosController {
 	@PutMapping("/{id}")
 	// dispara o update no banco de dados
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()) {
@@ -100,6 +104,7 @@ public class TopicosController {
 	}
 
 	@DeleteMapping("/{id}")
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optional = topicoRepository.findById(id);
 		if(optional.isPresent()) {
