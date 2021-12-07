90e2770b6cd5bb5a752d8ac20baf0bb2a5948f0e
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-06 22:48:05 -0300

    ordenação de tópicos

--- src/main/java/com/alura/forum/controller/TopicosController.java
+++ src/main/java/com/alura/forum/controller/TopicosController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -45,9 +46,10 @@ public class TopicosController {
 	public Page<TopicoDto> lista(
 			@RequestParam(required = false) String nomeCurso,
 			@RequestParam int pagina,
-			@RequestParam int qtdElementos) {
+			@RequestParam int qtdElementos,
+			@RequestParam String ordenacao) {
 
-		Pageable paginacao = PageRequest.of(pagina, qtdElementos);
+		Pageable paginacao = PageRequest.of(pagina, qtdElementos, Direction.ASC, ordenacao);
 		
 		if (nomeCurso == null) {
 			//troca de List para Page
