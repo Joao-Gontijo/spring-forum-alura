3bc0e60745471c802ff43413b073293669ce20ef
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-07 16:08:30 -0300

    liberando acesso a get topicos

--- src/main/java/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,5 +12,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
+	//configurações de autenticação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
 	
+	//configurações de autorização (perfis de acesso)
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+			//para liberar outras, continua com antMatchers
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+	
+	//configurações de recursos estáticos (js, css, imagens, etc)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
 }
