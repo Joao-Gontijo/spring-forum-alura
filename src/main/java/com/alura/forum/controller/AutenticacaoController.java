8be817a035d7476d39bf613fe6108fa55ab7471a
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-08 16:32:23 -0300

    configurando autenticação stateless

---
+++ src/main/java/com/alura/forum/controller/AutenticacaoController.java
@@ -0,0 +1,22 @@
+package com.alura.forum.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alura.forum.controller.form.LoginForm;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+	
+	@PostMapping
+	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
+		System.out.println(form.getEmail() + "\n" + form.getSenha());
+		return ResponseEntity.ok().build();
+	}
+}
