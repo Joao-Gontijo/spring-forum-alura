7a20fcfc03aaf2425bb0a2d004d9a879f5cdd926
Joao-Gontijo <jvgontijo166@gmail.com>
2021-12-08 16:59:24 -0300

    gerando token jwt

---
+++ src/main/java/com/alura/forum/config/security/TokenService.java
@@ -0,0 +1,40 @@
+package com.alura.forum.config.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import com.alura.forum.modelo.Usuario;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenService {
+
+	@Value("${forum.jwt.expiration}")
+	private String expiration;
+	
+	@Value("${forum.jwt.secret}")
+	private String secret;
+	
+	public String gerarToken(Authentication authentication) {
+		
+		Usuario logado = (Usuario) authentication.getPrincipal(); //retorna o usuário logado
+		Date hoje = new Date();
+		@SuppressWarnings("deprecation")
+		Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+		
+		return Jwts.builder()
+				.setIssuer("API Fórum Alura") //quem é que tá gerando o token
+				.setSubject(logado.getId().toString()) //quem é o usuário dono do token
+				.setIssuedAt(hoje) //data
+				.setExpiration(dataExpiracao) //tempo que o token vai durar
+				.signWith(SignatureAlgorithm.HS256, secret)
+				.compact();
+	}
+
+	
+}
